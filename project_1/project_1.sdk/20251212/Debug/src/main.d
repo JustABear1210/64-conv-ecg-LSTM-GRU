7985296f194e7109377a36cf8ad625bbc570a6e9
eason <eason12102001@gmail.com>
2026-02-04 16:12:44 +0800

    在原先的64bits conv裡面新增LSTM, GRU推論模型

---
+++ project_1/project_1.sdk/20251212/Debug/src/main.d
@@ -0,0 +1,85 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xuartps_hw.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xscutimer.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xscutimer_hw.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xdmaps.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xdmaps_hw.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xtime_l.h ../src/weights_gru.h \
+ ../src/weights_lstm.h ../src/rr_signal_1000.h ../src/rr_symbol_1000.h
+
+../../20251212_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xscutimer.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xscutimer_hw.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xdmaps.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xdmaps_hw.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xtime_l.h:
+
+../src/weights_gru.h:
+
+../src/weights_lstm.h:
+
+../src/rr_signal_1000.h:
+
+../src/rr_symbol_1000.h:
---
+++ project_1/project_1.sdk/20251212/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../20251212_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../20251212_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../20251212_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
